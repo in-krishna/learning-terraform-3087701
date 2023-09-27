f22e974b3a37953ef55b2c26b68bb62207cca0a2
in-krishna <143412661+in-krishna@users.noreply.github.com>
2023-09-27 12:39:44 +0530

    Variable Instance_type Changed

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"] 
   }
 
   filter {
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = variable.instance_type
 
   tags = {
     Name = "HelloWorld"
